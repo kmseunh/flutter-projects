a56ec0fd15a861100c2e37801ef8135d30346e6b
kmseunh <tmdgus8779@gmail.com>
2024-02-26 23:36:18 +0900

    feat: 통화 변환 UI를 위한 CurrencyConverterWidget 구현
    - 입력 필드, 변환 로직 및 디스플레이를 사용하여 CurrencyConverterWidgetStatefulWidget 생성
    - 라벨에는 상수를 사용하고 통화 변환 로직에는 변환기 사용

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blueGrey,
         scaffoldBackgroundColor: Colors.blueGrey,
       ),
-      home: CurrencyConverterPage(),
+      home: const CurrencyConverterPage(),
     );
   }
 }
