2b1511a4c5232fe65a9cae9a63d53f465160ad9e
kmseunh <tmdgus8779@gmail.com>
2024-02-26 18:03:42 +0900

    feat: 앱 상수에 대한 상수 클래스 추가
    - 앱 제목, 통화 레이블, 입력 필드 힌트 텍스트 및 변환 버튼 레이블에 대한 상수를 정의합니다.

---
+++ currency_converter/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String appTitle = 'Currency Converter';
+  static const String currencyLabel = 'INR';
+  static const String hintText = 'Please enter the amount in USD';
+  static const String convertButtonLabel = 'Convert';
+}
